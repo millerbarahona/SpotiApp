02a336e55d4b36e159881c36b1034f3e404cf36d
Miller Barahona <mbarahona@robinfood.com>
2021-12-14 17:00:49 -0500

    added query params

--- src/pages/SpotiApp.jsx
+++ src/pages/SpotiApp.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link, useParams, useSearchParams } from 'react-router-dom';
 import { Song } from '../components/Song';
 import useSongs from '../hooks/useSongs';
@@ -6,7 +6,6 @@ import useToken from '../hooks/useToken'
 import styles from '../styles/Container.module.css';
 
 export const SpotiApp = () => {
-  const params = useParams()
   const [query, setQuery] = useState('');
   const [token] = useToken();
   const [songs, loading, error] = useSongs(token, query);
@@ -17,13 +16,20 @@ export const SpotiApp = () => {
     const q = inputRef.current.value
     setSearchParams({q})
     clearTimeout(interval)
-    interval = setTimeout(() => setQuery(inputRef.current.value), 400)
+    interval = setTimeout(() => setQuery(inputRef.current.value), 1000)
   }
+
+  useEffect(() => {
+    const q = searchParams.get('q')
+    if (q) {
+      setQuery(q)
+    }
+  }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles.input_div}>
-        <input className={styles.input} type="text" onChange={handleTip} ref={inputRef} placeholder='Ingrese una canción etc' value={searchParams.get('q')}/>
+        <input className={styles.input} type="text" onChange={handleTip} ref={inputRef} placeholder='Ingrese una canción etc' value={searchParams.get('q') || ''}/>
         <div className={styles.logo_container}>
           <img src="https://cdn-icons-png.flaticon.com/512/49/49097.png" alt="" className={styles.logo} />
         </div>
